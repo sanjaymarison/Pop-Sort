0d1245fba92b6fc79d2416f8f1595e0a2a6f27c6
sanjay marison <61217970+sanjaymarison@users.noreply.github.com>
2021-04-05 08:07:06 +0530

    final script
    changed pop sort testing to work like a proper module

--- algorithm.py
+++ algorithm.py
@@ -1,20 +1,30 @@
+
+try:
+  import time
+  from statistics import mean
+except ImportError as e:
+  print("Testing Might not be available due to Import Error")
+  print(e)
 #my algorithm to sort a list in python
 #faster than bubble and insertion (only tested with these two)
 def pop_sort(list1):
   list2 =[]
-      while list1 != []:
-          list2.append(list1.pop(list1.index(min(list1))))
-   return list2
+  while list1 != []:
+    list2.append(list1.pop(list1.index(min(list1))))
+  return list2
       
-#mean time taken to sort for this algorithm code for hundred thousand iterations
-import time
-from statistics import mean
-time_list = []
-for i in range(100000):
-    start = time.time()
-    list1,list2 = [7,5,3,5,8,2,4,3],[]
-    while list1 != []:
+def test(list_=[7,5,3,5,8,2,4,3],cycles=100000): #mean time taken to sort for this algorithm code for hundred thousand iteration
+  time_list = []
+  given_list = list_.copy()
+  for i in range(cycles):
+      start = time.time()
+      list1,list2 = list_,[]
+      while list1 != []:
         list2.append(list1.pop(list1.index(min(list1))))
-    end = time.time()
-    time_list.append((end-start))
-print(mean(time_list))
+      end = time.time()
+      time_list.append((end-start))
+  print()
+  print("Mean time taken to cycle through",cycles,"iterations for",given_list," is ",mean(time_list))
+  print("Total time taken to cycle through:",sum(time_list))
+  print()
+test()
